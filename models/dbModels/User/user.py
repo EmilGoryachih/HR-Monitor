fc53368612261336a51c3dbbd4e650880bf323ab
emil <emil12.05.2005@gmail.com>
2024-10-04 15:48:52 +0300

    JWT

--- models/dbModels/User/user.py
+++ models/dbModels/User/user.py
@@ -1,11 +1,13 @@
 import uuid
 from sqlalchemy import Column, String, Date, Enum as SQLAEnum
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
 from enum import Enum
+from passlib.context import CryptContext
 
 from models.base import BaseModel
-from sqlalchemy import Table, ForeignKey, Integer
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 # Enum for user roles
@@ -14,40 +16,24 @@ class RoleEnum(str, Enum):
     EMPLOYEE = "employee"
 
 
-# Table for many-to-many relationship between users and permissions
-user_permissions = Table(
-    'user_permissions', BaseModel.metadata,
-    Column('user_id', UUID(as_uuid=True), ForeignKey('users.id')),
-    Column('permission_id', Integer, ForeignKey('permissions.id'))
-)
-
-
-class PermissionModel(BaseModel):
-    __tablename__ = 'permissions'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-
-
 class UserModel(BaseModel):
     __tablename__ = 'users'
 
-    # Existing fields
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True, nullable=False)
     name = Column(String(length=50), index=True, nullable=False)
     surname = Column(String(length=50), index=True, nullable=True)
     phone = Column(String(length=15), index=True, nullable=True)
-    email = Column(String(length=50), index=True, nullable=True)
+    email = Column(String(length=50), index=True, nullable=False)
+    password = Column(String(length=100), nullable=False)
     birth_date = Column(Date, index=True, nullable=True)
-
-    # New role field
     role = Column(SQLAEnum(RoleEnum), default=RoleEnum.EMPLOYEE, nullable=False)
 
-    # Relationships
-    permissions = relationship("PermissionModel", secondary=user_permissions, back_populates="users")
+    def set_password(self, password: str):
+        self.password = pwd_context.hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.password)
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, role={self.role!r})"
 
-
-PermissionModel.users = relationship("UserModel", secondary=user_permissions, back_populates="permissions")
