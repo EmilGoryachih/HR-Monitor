a773f98196649fb31dd27a7f8c90406e7cd0cf77
emil <emil12.05.2005@gmail.com>
2024-10-05 23:19:54 +0300

    Resume crud

--- api/routes/resume.py
+++ api/routes/resume.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from .auth import get_current_user
 from db.session import fastapi_get_db
 from models.dtoModels.resumeDTO import ResumeDTO
-from models.dbModels.Resume.crud import create_resume
+from models.dbModels.Resume.crud import create_resume, get_all_resumes, get_resume_by_id, change_resume, delete_resume
 
 from models.dbModels.User.crud import UserBasicResponse
 
@@ -24,4 +24,56 @@ async def create_vacancy_endpoint(vacancy: ResumeDTO, db: AsyncSession = Depends
 
     except Exception as e:
         print(f"Error while creating resume: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+@router.get("/all")
+async def get_all_user_resumes_endpoint(db: AsyncSession = Depends(fastapi_get_db), current_user: UserBasicResponse = Depends(get_current_user)):
+    try:
+        resumes = await get_all_resumes(db, current_user.id)
+        return resumes
+
+    except Exception as e:
+        print(f"Error while getting all resumes: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+
+@router.get("/{resume_id}")
+async def get_resume_endpoint(resume_id: uuid.UUID, db: AsyncSession = Depends(fastapi_get_db), current_user: UserBasicResponse = Depends(get_current_user)):
+    try:
+        resume = await get_resume_by_id(db, current_user.id, resume_id)
+        return resume
+
+    except HTTPException as http_ex:
+        raise http_ex
+
+    except Exception as e:
+        print(f"Error while getting resume: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+
+@router.put("/{resume_id}")
+async def update_resume_endpoint(resume_id: uuid.UUID, resume_data: ResumeDTO, db: AsyncSession = Depends(fastapi_get_db), current_user: UserBasicResponse = Depends(get_current_user)):
+    try:
+        updated_resume = await change_resume(db, resume_data, current_user.id, resume_id)
+        return updated_resume
+
+    except HTTPException as http_ex:
+        raise http_ex
+
+    except Exception as e:
+        print(f"Error while updating resume: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+
+@router.delete("/{resume_id}")
+async def delete_resume_endpoint(resume_id: uuid.UUID, db: AsyncSession = Depends(fastapi_get_db), current_user: UserBasicResponse = Depends(get_current_user)):
+    try:
+        result = await delete_resume(db, current_user.id, resume_id)
+        return result
+
+    except HTTPException as http_ex:
+        raise http_ex
+
+    except Exception as e:
+        print(f"Error while deleting resume: {e}")
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
--- models/dbModels/Resume/crud.py
+++ models/dbModels/Resume/crud.py
@@ -1,5 +1,10 @@
 import uuid
+
+from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
 from models.dbModels.Resume.resume import ResumeModel
 from models.dtoModels.resumeDTO import ResumeDTO
 
@@ -32,3 +37,60 @@ async def create_resume(db: AsyncSession, resume_dto: ResumeDTO, user_id: uuid.U
     await db.refresh(new_resume)
 
     return new_resume
+
+
+async def get_all_resumes(db: AsyncSession, user_id: uuid.UUID):
+    statement = select(ResumeModel).where(ResumeModel.user_id == user_id)
+    result = await db.execute(statement)
+    resumes = result.scalars().all()
+
+    return resumes
+
+
+async def get_resume_by_id(db: AsyncSession, user_id: uuid.UUID, resume_id: uuid.UUID):
+    statement = select(ResumeModel).where(ResumeModel.id == resume_id, ResumeModel.user_id == user_id)
+    result = await db.execute(statement)
+    resume = result.scalar_one_or_none()
+
+    if resume is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resume not found or not authorized to view")
+
+    return resume
+
+
+async def change_resume(db: AsyncSession, resume_dto: ResumeDTO, user_id: uuid.UUID, resume_id: uuid.UUID):
+    statement = select(ResumeModel).where(ResumeModel.id == resume_id, ResumeModel.user_id == user_id)
+    result = await db.execute(statement)
+    resume = result.scalar_one_or_none()
+
+    if resume is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resume not found or not authorized to update")
+
+    resume.name = resume_dto.name
+    resume.study = resume_dto.study.dict() if resume_dto.study else None
+    resume.desired_busyness = resume_dto.desired_busyness
+    resume.specialization = resume_dto.specialization
+    resume.skills = resume_dto.skills
+    resume.about_me = resume_dto.about_me
+    resume.courses = [course.dict() for course in resume_dto.courses] if resume_dto.courses else None
+    resume.work_experience = [work.dict() for work in resume_dto.work_experience] if resume_dto.work_experience else None
+
+    await db.commit()
+    await db.refresh(resume)
+
+    return resume
+
+
+async def delete_resume(db: AsyncSession, user_id: uuid.UUID, resume_id: uuid.UUID):
+    statement = select(ResumeModel).where(ResumeModel.id == resume_id, ResumeModel.user_id == user_id)
+    result = await db.execute(statement)
+    resume = result.scalar_one_or_none()
+
+    if resume is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Resume not found or not authorized to delete")
+
+    await db.delete(resume)
+    await db.commit()
+
+    return {"detail": "Resume deleted successfully"}
